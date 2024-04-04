64e00c30aa4ec33ab1232313420c48ea97d23a00
AnanyaUmesh <117510792+AnanyaUmesh@users.noreply.github.com>
2024-04-04 21:51:57 +0530

    easy(stack)

---
+++ 1614. Maximum Nesting Depth of the Parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int n=s.length();
+        int ans=0;
+        stack<char> st;
+        for(char c: s){
+        if(c=='('){
+         st.push(c);
+         }
+        else if(c==')'){
+        st.pop();}
+        ans=max(ans,(int)st.size());
+        }
+        return ans;
+    }
+};
